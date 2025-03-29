d03276796ff125582b66b22d19e7d75683742359
Maham-Fatima <fmaham599@gmail.com>
2025-03-29 09:27:26 -0700

    added speed insights

--- tailwindcss4/src/component/Home.jsx
+++ tailwindcss4/src/component/Home.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion"
 import profile from "../assets/coder.jpg";
+import ResumeDownload from "./resumeDownload";
 import "../../src/index.css"
 export default function Home() {
   return (
@@ -28,15 +29,24 @@ export default function Home() {
           >
             A passionate Web Developer and AI Enthusiast having expertise in javascript and python. Driven by curiosity, I thrive on turning ideas into impactful digital experiences. I aim to create innovative solutions that blend technology and creativity to solve real-world problems and make a meaningful difference.
           </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 3 }}
+            className="p-2 text-slate-400"
+          >
+           <ResumeDownload/>
+          </motion.div>
+          
         </div>
         <div className="flex justify-center items-center md:col-span-5 mt-8 md:mt-0 ">
 
         <div className="relative p-5 rounded-full">
     {/* Asymmetric border effect */}
-    <div className="absolute top-0 left-0 w-[50%] h-[50%] border-t-4 border-l-4 border-yellow-500 rounded-full"></div>
-    <div className="absolute bottom-0 right-0 w-[80%] h-[80%] border-b-4 border-r-4 border-yellow-600 rounded-full"></div>
-    <div className="absolute top-0 right-0 w-[70%] h-[70%] border-t-4 border-l-4 border-cyan-400 rounded-full"></div>
-    <div className="absolute bottom-0 left-0 w-[50%] h-[50%] border-b-4 border-r-4 border-cyan-600 rounded-full"></div>
+    <div className="absolute top-0 left-0 w-[50%] h-[50%] border-t-4 border-l-4 border-yellow-500 rounded-full border-move"></div>
+    <div className="absolute bottom-0 right-0 w-[80%] h-[80%] border-b-4 border-r-4 border-yellow-600 rounded-full border-move"></div>
+    <div className="absolute top-0 right-0 w-[70%] h-[70%] border-t-4 border-l-4 border-cyan-400 rounded-full border-move"></div>
+    <div className="absolute bottom-0 left-0 w-[50%] h-[50%] border-b-4 border-r-4 border-cyan-600 rounded-full border-move"></div>
 
     <img
       src={profile}
